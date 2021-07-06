ac9e83e9918d5202c2416b390a75b33528e969bc
Aksy00 <63105982+Aksy00@users.noreply.github.com>
2021-07-06 19:16:19 +0300

    Update Tentro.py

--- Tentro.py
+++ Tentro.py
@@ -28,7 +28,8 @@ async def on_message(message):
     if message.content == '!thelp':
       
       myEmbed = discord.Embed(title="These are all the commands", color=0xFF0000)
-      myEmbed.add_field(name="code version:", value="v1.0", inline=False)
+      myEmbed.add_field(name="available commands:", value="Clear", inline=False)
+      myEmbed.add_field(name="bot version:", value="v1.0", inline=False)
       myEmbed.add_field(name="Date released:", value="July 6th", inline=False)
       myEmbed.set_footer(text="Still in progress!")
       myEmbed.set_author(name=message.author.name)
@@ -47,7 +48,7 @@ async def on_message(message):
       await message.channel.send("Hello There")   
       await client.process_commands(message)
     
-@client.command('clear')
+@client.command()
 @commands.has_permissions(manage_messages = True)
 async def purge(ctx, amount = 3):
   await ctx.channel.purge(limit = amount)

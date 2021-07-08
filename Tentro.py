652d0b9774bc35bc66b5db409f393db12fff3352
Aksy00 <ilievaleks007@gmail.com>
2021-07-08 17:05:04 +0300

    tried to do if statements for some commands so if they dont have perms bot replies

--- Tentro.py
+++ Tentro.py
@@ -74,26 +74,30 @@ async def kick(ctx, member : discord.Member, *, reason=None):
   embed = discord.Embed(title = (f"You have been kicked from: {guild.name}.\n**Reason:** {reason}."), colour=discord.Color(0xff0000))
   await member.send(embed=embed)
     
-@client.command(name='m')   
-@commands.has_permissions(manage_messages=True)
+@client.command(name='m') 
+@commands.has_permissions(administrator=True)  
 async def mute(ctx, member: discord.Member, *, reason=None):
     guild = ctx.guild
     mutedRole = discord.utils.get(guild.roles, name="Muted")
-
+   
+  
     if not mutedRole:
-        mutedRole = await guild.create_role(name="Muted", colour=discord.Colour(0x34eb40))
+      mutedRole = await guild.create_role(name="Muted", colour=discord.Colour(0x34eb40))
 
-        for channel in guild.channels:
+      for channel in guild.channels:
             await channel.set_permissions(mutedRole, speak=False, send_messages=False, read_message_history=True, read_messages=False)
-    embed = discord.Embed(title="Muted", description=f"{member.mention} has been muted indefinitely.", colour=discord.Colour(0xff0000))
-    embed.add_field(name="Reason:", value=reason, inline=False)
-    embed.set_footer(text="Mute")
-    embed.timestamp = datetime.datetime.now()
-    await ctx.send(embed=embed)
-    await member.add_roles(mutedRole, reason=reason)
-    embed = discord.Embed(title = (f"You have been muted in: {guild.name}.\n**Reason:** {reason}."), colour=discord.Color(0xff0000))
-    await member.send(embed=embed)
+      embed = discord.Embed(title="Muted", description=f"{member.mention} has been muted indefinitely.", colour=discord.Colour(0xff0000))
+      embed.add_field(name="Reason:", value=reason, inline=False)
+      embed.set_footer(text="Mute")
+      embed.timestamp = datetime.datetime.now()
+      await ctx.send(embed=embed)
+      await member.add_roles(mutedRole, reason=reason)
+      embed = discord.Embed(title = (f"You have been muted in: {guild.name}.\n**Reason:** {reason}."), colour=discord.Color(0xff0000))
+      await member.send(embed=embed)
     
+
+
+
 @client.command(name='clear')
 @commands.has_permissions(manage_messages = True)
 async def purge(ctx, amount: int):
@@ -101,18 +105,19 @@ async def purge(ctx, amount: int):
   await ctx.send(f"{ctx.author.mention}, purged {amount} message(s)")
 
 @client.command(description="Unmutes a specified user.")
-@commands.has_permissions(manage_messages=True)
 async def unmute(ctx, member: discord.Member):
    guild = ctx.guild
    mutedRole = discord.utils.get(ctx.guild.roles, name="Muted")
-
-   await member.remove_roles(mutedRole)
-   embed = discord.Embed(title="Unmuted", description=f"{member.mention} has been unmuted.",colour=discord.Colour(0xff0000))
-   embed.set_footer(text='Unmute')
-   embed.timestamp = datetime.datetime.now()
-   await ctx.send(embed=embed)
-   embed = discord.Embed(title = (f"**You have been unmuted in: {guild.name}.**"), colour=discord.Color(0xff0000))
-   await member.send(embed=embed)
+   if ctx.author.guild_permissions.manage_messages:
+    await member.remove_roles(mutedRole)
+    embed = discord.Embed(title="Unmuted", description=f"{member.mention} has been unmuted.",colour=discord.Colour(0xff0000))
+    embed.set_footer(text='Unmute')
+    embed.timestamp = datetime.datetime.now()
+    await ctx.send(embed=embed)
+    embed = discord.Embed(title = (f"**You have been unmuted in: {guild.name}.**"), colour=discord.Color(0xff0000))
+    await member.send(embed=embed)
+   else:
+     await ctx.channel.send("You dont have the required permissions to do that!", delete_after=5)
 
 @client.command(name='ping')
 async def ping(ctx, arg=None):
@@ -136,8 +141,8 @@ async def server(ctx, arg=None):
   await ctx.channel.send(embed=embed)
 
 @client.command(name='mutetimer')
-@commands.has_permissions(manage_messages=True)
 async def mute(ctx, member: discord.Member,time):
+  if ctx.author.guild_permissions.manage_messages:
     muted_role=discord.utils.get(ctx.guild.roles, name="Muted")
     time_convert = {"s":1, "m":60, "h":3600,"d":86400}
     tempmute= float(time[0]) * time_convert[time[-1]] 
@@ -146,6 +151,8 @@ async def mute(ctx, member: discord.Member,time):
     await ctx.send(embed=embed)
     await asyncio.sleep(tempmute)
     await member.remove_roles(muted_role)
+  else:
+    await ctx.channel.send("You dont have the required permissions to do that!", delete_after=5)
 
 
 if __name__ == '__main__':

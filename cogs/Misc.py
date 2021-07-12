9a63f2f2dd7e41a803091ebeda0ad6e97a4756c5
Aksy00 <ilievaleks007@gmail.com>
2021-07-12 22:23:34 +0300

    added probably not working welcome command that needs fixing

---
+++ cogs/Misc.py
@@ -0,0 +1,75 @@
+from discord.ext import commands
+from discord import Embed, Member, User, utils
+import asyncio
+import discord, random
+from discord.ext.commands import bot
+import discord, datetime
+from discord import *
+from discord.ext import commands
+import os, random
+from pathlib import Path
+
+
+class Misc(commands.Cog):
+
+    def __init__(self, bot): 
+        self.bot = bot
+
+    @commands.command(name="8ball")
+    async def _8ball(ctx, *, question=None):
+        responses = ["Definitely.", "It is certain", "Does 2 + 2 equal to 4?", "I don't think so chief.",
+                "Perhaps.",
+                "Maybe, ehhh don't take my word for it.",
+                "Ask again.",
+                "How do you not know this.", "I don't know, im just a discord bot.",
+                "No clue bro.",
+                "Uhhh Not sure about the answer to that one.", "My reply is no.",
+                "My sources say no.",
+                "Outlook not so good.",
+                "Very doubtful.", "My sources say yes."]
+
+        if question == None:
+            await ctx.send("Please ask a question.", delete_after=5)
+        else:
+            embed = discord.Embed(title=f"**Question:**\n", description = f"{question}", color=0xff0000)
+            embed.add_field(name = f"**8ball:**\n" ,value = f"{random.choice(responses)}")
+            await ctx.send(embed=embed)
+
+    
+    
+
+    @commands.command(name='welcome')
+    async def welcome(self, ctx):
+        guild = ctx.guild
+        member_join = guild.join
+        welcome_channel = bot.get_channel("Welcome")
+        if ctx.author.has_permissions.administrator:
+            await guild.create_text_channel("Welcome")
+            embed = discord.Embed(title=f"Channel  has been created! Members who join will now be welcomed there.")
+            embed.timestamp = ctx.message.created_at
+            await ctx.send(embed=embed)
+
+        else:
+            embed = Embed(title="You do not have the required permissions to do that!", colour=(0xff0000))
+            await ctx.send(embed=embed, delete_after=5)
+
+        if member_join:
+            embed = discord.Embed(title=f"{member.mention} has joined the server!")
+            await welcome_channel.send(embed=embed)
+    
+            
+
+        
+
+
+
+
+
+
+
+
+
+
+
+def setup(bot):
+    bot.add_cog(Misc(bot))

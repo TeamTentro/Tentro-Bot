e0a342d3ae9ec858944625a6864ba1d6f992aade
Aksy00 <ilievaleks007@gmail.com>
2021-08-08 17:33:44 +0300

    db stuff

--- cogs/AutoMod.py
+++ cogs/AutoMod.py
@@ -5,7 +5,7 @@ from discord import Embed, Member, User, channel, client, colour, guild, message
 import asyncio, discord
 from discord.ext.commands import bot
 from discord.ext.commands.errors import MissingPermissions
-import random
+import random, Toggle
 from typing import Dict, List, Pattern, Set, Tuple, Union
 import re, unicodedata
 import cmath as math
@@ -20,7 +20,7 @@ DEACTIVATED_COLOR = 0xff0000
 RED = 0xff0000
 DELETE_TIME: float = 5
 
-_BLACK_LIST = ["dood"]
+_BLACK_LIST = ["dood", "nigga", "nigger", "faggot"]
 _FILLERS = [" ", "\-", "_"]
 
 
@@ -35,31 +35,54 @@ class AutoMod(commands.Cog):
 
     activated: bool
     blacklist: List[str]
-    Toggle: bool
+    
 
+
+    
+    
+
+# Example event
+
+
+  
+    
     Toggle = False
     @commands.command(name="automod")
     async def _automod(self, ctx):
+        
         if not eligible(ctx.author):
-            return
-
-        global Toggle       
-        Toggle = True
-    
-
-        if not eligible(ctx.author):
+            await ctx.send("You do not have the required permissions to do that!", delete_after=5)
             return
         
+        global Toggle   
+        await ctx.message.add_reaction("✅")
+        if Toggle is not True and Toggle is not False:
+            Toggle = False
             
         
-        await ctx.message.add_reaction("✅")
+    
+
+        Toggle = not Toggle
+
+        if Toggle:
+            embed = Embed(title="Automod has been enabled!", color=RED)
+            await ctx.send(embed=embed)
+        elif not Toggle:
+            embed = Embed(title="Automod has been disabled!", color=RED)
+            await ctx.send(embed=embed)
+
+        
                
 
     @commands.Cog.listener()
     async def on_message(self, message):
-  
+
+        if eligible(message.author):
+            return
+ 
         if Toggle == True:
             try:
+                
                 message = message
                 bl_words = mod.check_bl(str(message.content), _BLACK_LIST, bl_algorithms=[
                 mod.check_bl_direct(), mod.check_bl_fillers()], fillers=_FILLERS)
@@ -73,16 +96,25 @@ class AutoMod(commands.Cog):
                 spam_probability = mod.get_spam_probability(str(message.content), spam_algorithms=[mod.check_spam_alternating_cases(
                 ), mod.check_spam_by_repetition(), mod.check_spam_repeating_letters(), mod.check_spam_caps()])
                 if spam_probability > 0.5:
-                    embed = discord.Embed(title="Dont spam in this channel!")
+                    embed = discord.Embed(title="Don't spam in this channel!")
                     await message.channel.send(embed=embed, delete_after=3)  
                     await message.delete()             
                 
             except:
                 pass
-       
+
+        elif Toggle is False: 
+            pass
 
 def eligible(member: Member) -> bool:
     return member.guild_permissions.administrator
+
+async def bot_activation(self, activated: bool, ctx):
+    color, activation_text = (ACTIVATED_COLOR, "Activated") if activated else(
+        DEACTIVATED_COLOR, "Deactivated")
+    embed = Embed(title=f"Automod {activation_text}", color=color)
+    await bot_activation(self.activated, ctx)
+    await ctx.send(embed = embed, delete_after=DELETE_TIME)
 
 
    

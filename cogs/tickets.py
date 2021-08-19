07c384bb6a903a6fd0636378830bf932f2398ebd
Aksy00 <ilievaleks007@gmail.com>
2021-08-19 17:25:58 +0300

    a lot of stuff for tickets

--- cogs/tickets.py
+++ cogs/tickets.py
@@ -1,42 +1,37 @@
 
 from discord import Embed
 from discord.ext import commands
-import discord.utils
+import discord.utils, sqlite3
 
 
 from operator import is_not, not_
 from discord import Embed, Member, User, channel, client, colour, guild, message, user, utils
 
-from discord.ext import commands
-import asyncio, discord
-from discord.ext.commands import bot
-from discord.ext.commands.errors import MissingPermissions
-import random
-from typing import Dict, List, Pattern, Set, Tuple, Union
-import re, unicodedata
-import cmath as math, sqlite3
-from typing import List
-
-from typing import Dict, List, Tuple, Union
 class tickets(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
 
 
-   
+    
 
 
 
 
     @commands.command(name="help_tickets")
     async def help_ticket(self, ctx):
-        embed = Embed(title="Tickets", description="ticket_add: Creates a ticket channel and category\nticket_remove: Deletes the ticket channel and category\nticket_addrole: Adds a role that will be able to see the tickets\nticket_removerole: Removes the role's permissions  to view tickets", colour=0xff0000)
+        embed = Embed(title="Tickets", description="ticket_add: Creates a ticket channel and category, you can also add a custom message to it, if not it will use a default message we have set\nticket_remove: Deletes the ticket channel and category\nticket_addrole: Adds a role that will be able to see the tickets\nticket_removerole: Removes the role's permissions  to view tickets", colour=0xff0000)
         await ctx.send(embed=embed)
 
     @commands.command(name="tickets_add")
     @commands.is_owner()
-    async def tickets_add(self, ctx):
+    async def tickets_add(self, ctx, *, text=None):
+        db = sqlite3.connect('tentro.sqlite')
+        cursor = db.cursor()
+        cursor.execute(f"SELECT msg FROM ticket WHERE guild_id = {ctx.guild.id}")
+        result = cursor.fetchone()
+       
+        
 
         # Check if the category already exists
         ticketcat_e = discord.utils.get(ctx.guild.categories, name="🎫-Tickets")
@@ -46,21 +41,44 @@ class tickets(commands.Cog):
             ticketcat = await ctx.guild.create_category(name="🎫-Tickets")
 
         # Check if the text channel already exists
-        ticketchannel_e = discord.utils.get(ctx.guild.text_channels, name="tickets")
+        ticketchannel_e = discord.utils.get(ctx.guild.text_channels, name="ticketinfo")
         if ticketchannel_e:
             return await ctx.send("Ticket channel already exists, setup aborted.")
         else:
-            ticketchannel = await ctx.guild.create_text_channel(name="tickets", category=ticketcat)
+            ticketchannel = await ctx.guild.create_text_channel(name="ticketinfo", category=ticketcat)
 
         default_role = ctx.guild.default_role
         await ticketchannel.set_permissions(target=default_role, speak=False, send_messages=False, read_message_history=True, read_messages=True, add_reactions=False)
+        if text:
+            embedticket = Embed(title=f"Tickets for {ctx.guild.name}", description=f"{text}", color =0xff0000)
+            ticketmsg = await ticketchannel.send(embed=embedticket)
+            await ticketmsg.add_reaction("🎫")
+            addedembed = Embed(title="✅| Succesfully added the Tentro ticket system to this server. Run t!help_tickets for more info.", colour = 0x00ff00)
+            await ctx.send(embed=addedembed)
+            
+            
+            
 
-        embedticketchannel = Embed(title=f"Tickets for {ctx.guild.name}", description="React to this message with '🎫' to open a ticket. Use tickets wisely and don't open them for dumb reasons.", color=0xc4f21d)
-        ticketmsg = await ticketchannel.send(embed=embedticketchannel)
-        await ticketmsg.add_reaction("🎫")
+        elif text==None:
+            embedticketchannel = Embed(title=f"Tickets for {ctx.guild.name}", description="React to this message with '🎫' to open a ticket. Use tickets wisely and don't open them for dumb reasons.", color=0xc4f21d)
+            ticketmsg = await ticketchannel.send(embed=embedticketchannel)
+            await ticketmsg.add_reaction("🎫")
 
-        addedembed = Embed(title="✅| Succesfully added the Tentro ticket system to this server. Run t!help_tickets for more info.", colour = 0x00ff00)
-        await ctx.reply(embed=addedembed)
+            addedembed = Embed(title="✅| Succesfully added the Tentro ticket system to this server. Run t!help_tickets for more info.", colour = 0x00ff00)
+            await ctx.reply(embed=addedembed)
+
+        if result and text==None:
+            pass
+
+
+        if result:
+            sql = ("UPDATE ticket SET msg = ? WHERE guild_id = ?")
+            val = (text, ctx.guild.id)
+            await ctx.reply(f"Ticket message has been set!")
+        cursor.execute(sql, val)
+        db.commit()
+        cursor.close()
+        db.close()
 
       
 
@@ -70,7 +88,7 @@ class tickets(commands.Cog):
     async def tickets_remove(self, ctx):
         
         ticketcategory = discord.utils.get(ctx.guild.categories, name="🎫-Tickets")
-        ticketchannel = discord.utils.get(ctx.guild.text_channels, name="tickets")
+        ticketchannel = discord.utils.get(ctx.guild.text_channels, name="ticketinfo")
         if ticketcategory == None or ticketchannel == None:
             embed = Embed(title="✅| There are no ticket utils", colour = 0x00ff00)
             await ctx.reply(embed=embed)
@@ -81,18 +99,60 @@ class tickets(commands.Cog):
           embed = Embed(title="✅| Successfully removed the ticket utilities from this server", colour = 0x00ff00)
           await ctx.reply(embed=embed)
 
+    @commands.command(name="roleperm")
+    @commands.is_owner()
+    async def roleperm(self, ctx, role: discord.Role):
+        db = sqlite3.connect('tentro.sqlite')
+        cursor = db.cursor()
+        cursor.execute(f"SELECT role_id FROM ticket WHERE guild_id = {ctx.guild.id}")
+        result = cursor.fetchone()
+        if result is None:
+            sql = ("INSERT INTO ticket(guild_id, role_id) VALUES(?,?)")
+            val = (ctx.guild.id, role)
+            await ctx.send(f"Role_id has been set to {role}")
+        elif result is not None:
+            sql = ("UPDATE ticket SET role_id = ? WHERE guild_id = ?")
+            val = (role, ctx.guild.id)
+            await ctx.send(f"Role_id has been updated to {role}") 
+        cursor.execute(sql, val)
+        db.commit()
+        cursor.close()
+        db.close()
+        
+            
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g : g.id == guild_id, self.bot.guilds)
+        emoji = payload.emoji.name
+        name = payload.member.name
+        member = payload.member
+        role2 = discord.utils.get(guild.roles, name="@everyone")
+        channelname = 877906394530078770
+        
+        if payload.message_id == 877906396216176700:
+  
+            
+
+            role_id = 745925853229350972
+            role = guild.get_role(role_id)
+            ticketcat_e = discord.utils.get(guild.categories, name="🎫-Tickets")
+        if member is not None:
+            tick = await guild.create_text_channel(name=f"ticket-{name}", category=ticketcat_e)
+            await tick.set_permissions(target=role2, speak=False, send_messages=False, read_message_history=False, read_messages=False, add_reactions=False, view_channel=False)
+         
+            
+
 
         guild_id = payload.guild_id
-        guild = self.client.get_guild(guild_id)
+        guild = self.bot.get_guild(guild_id)
 
         user_id = payload.user_id
-        user = self.client.get_user(user_id)
+        user = self.bot.get_user(user_id)
 
         channel_id = payload.channel_id
-        channel = self.client.get_channel(channel_id)
+        channel = self.bot.get_channel(channel_id)
 
         message_id = payload.message_id
         emoji = payload.emoji.name
